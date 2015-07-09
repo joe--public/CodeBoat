e0374f4cd13deca70ab2c0dd8d6733840a63a6a8
jo.nineo <jo.nineo@gmail.com>
2015-07-09 15:52:24 +0100

    initial customer code

---
+++ CodeBoat.TestBed.WebServices/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBoat.TestBed.WebServices
+{
+    public class Customer: ICustomer
+    {
+        public string GetCustomerName(int customerId)
+        {
+            return string.Format("{0}, New Customer Name", customerId);
+        }
+    }
+}
