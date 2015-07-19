176837d6cdd3182475e8c7e2fbe90c00779fc913
joe--public <jo.nineo@gmail.com>
2015-07-19 14:46:24 +0100

    Simple windows application to test commit and push to git.

---
+++ Lsg.WinApp.UI/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Lsg.WinApp.UI
+{
+    partial class FormLsgWinApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelButton = new System.Windows.Forms.Panel();
+            this.panelExtra = new System.Windows.Forms.Panel();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.groupBoxMainDetail = new System.Windows.Forms.GroupBox();
+            this.textBox04 = new System.Windows.Forms.TextBox();
+            this.textBoxTest03 = new System.Windows.Forms.TextBox();
+            this.textBoxTest02 = new System.Windows.Forms.TextBox();
+            this.textBoxTest01 = new System.Windows.Forms.TextBox();
+            this.labelTest04 = new System.Windows.Forms.Label();
+            this.labelTest03 = new System.Windows.Forms.Label();
+            this.labelTest02 = new System.Windows.Forms.Label();
+            this.labelTest01 = new System.Windows.Forms.Label();
+            this.groupBoxExtraDetail = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panelButton.SuspendLayout();
+            this.panelExtra.SuspendLayout();
+            this.panelMain.SuspendLayout();
+            this.groupBoxMainDetail.SuspendLayout();
+            this.groupBoxExtraDetail.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelButton
+            // 
+            this.panelButton.Controls.Add(this.buttonUpdate);
+            this.panelButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelButton.Location = new System.Drawing.Point(0, 261);
+            this.panelButton.Name = "panelButton";
+            this.panelButton.Padding = new System.Windows.Forms.Padding(5, 0, 5, 5);
+            this.panelButton.Size = new System.Drawing.Size(706, 42);
+            this.panelButton.TabIndex = 2;
+            // 
+            // panelExtra
+            // 
+            this.panelExtra.Controls.Add(this.groupBoxExtraDetail);
+            this.panelExtra.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelExtra.Location = new System.Drawing.Point(350, 0);
+            this.panelExtra.Name = "panelExtra";
+            this.panelExtra.Padding = new System.Windows.Forms.Padding(2, 5, 5, 5);
+            this.panelExtra.Size = new System.Drawing.Size(356, 261);
+            this.panelExtra.TabIndex = 1;
+            // 
+            // panelMain
+            // 
+            this.panelMain.Controls.Add(this.groupBoxMainDetail);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Padding = new System.Windows.Forms.Padding(5, 5, 2, 5);
+            this.panelMain.Size = new System.Drawing.Size(350, 261);
+            this.panelMain.TabIndex = 0;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUpdate.Enabled = false;
+            this.buttonUpdate.Location = new System.Drawing.Point(623, 7);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 29);
+            this.buttonUpdate.TabIndex = 0;
+            this.buttonUpdate.Text = "&Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxMainDetail
+            // 
+            this.groupBoxMainDetail.Controls.Add(this.textBox04);
+            this.groupBoxMainDetail.Controls.Add(this.textBoxTest03);
+            this.groupBoxMainDetail.Controls.Add(this.textBoxTest02);
+            this.groupBoxMainDetail.Controls.Add(this.textBoxTest01);
+            this.groupBoxMainDetail.Controls.Add(this.labelTest04);
+            this.groupBoxMainDetail.Controls.Add(this.labelTest03);
+            this.groupBoxMainDetail.Controls.Add(this.labelTest02);
+            this.groupBoxMainDetail.Controls.Add(this.labelTest01);
+            this.groupBoxMainDetail.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxMainDetail.Location = new System.Drawing.Point(5, 5);
+            this.groupBoxMainDetail.Name = "groupBoxMainDetail";
+            this.groupBoxMainDetail.Size = new System.Drawing.Size(343, 251);
+            this.groupBoxMainDetail.TabIndex = 0;
+            this.groupBoxMainDetail.TabStop = false;
+            this.groupBoxMainDetail.Text = "Main Details";
+            // 
+            // textBox04
+            // 
+            this.textBox04.Location = new System.Drawing.Point(97, 123);
+            this.textBox04.MaxLength = 20;
+            this.textBox04.Name = "textBox04";
+            this.textBox04.Size = new System.Drawing.Size(215, 20);
+            this.textBox04.TabIndex = 7;
+            // 
+            // textBoxTest03
+            // 
+            this.textBoxTest03.Location = new System.Drawing.Point(97, 90);
+            this.textBoxTest03.MaxLength = 20;
+            this.textBoxTest03.Name = "textBoxTest03";
+            this.textBoxTest03.Size = new System.Drawing.Size(215, 20);
+            this.textBoxTest03.TabIndex = 5;
+            // 
+            // textBoxTest02
+            // 
+            this.textBoxTest02.Location = new System.Drawing.Point(97, 57);
+            this.textBoxTest02.MaxLength = 20;
+            this.textBoxTest02.Name = "textBoxTest02";
+            this.textBoxTest02.Size = new System.Drawing.Size(215, 20);
+            this.textBoxTest02.TabIndex = 3;
+            // 
+            // textBoxTest01
+            // 
+            this.textBoxTest01.Location = new System.Drawing.Point(97, 28);
+            this.textBoxTest01.MaxLength = 20;
+            this.textBoxTest01.Name = "textBoxTest01";
+            this.textBoxTest01.Size = new System.Drawing.Size(215, 20);
+            this.textBoxTest01.TabIndex = 1;
+            // 
+            // labelTest04
+            // 
+            this.labelTest04.AutoSize = true;
+            this.labelTest04.Location = new System.Drawing.Point(19, 130);
+            this.labelTest04.Name = "labelTest04";
+            this.labelTest04.Size = new System.Drawing.Size(72, 13);
+            this.labelTest04.TabIndex = 6;
+            this.labelTest04.Text = "Main Detail 4:";
+            // 
+            // labelTest03
+            // 
+            this.labelTest03.AutoSize = true;
+            this.labelTest03.Location = new System.Drawing.Point(19, 97);
+            this.labelTest03.Name = "labelTest03";
+            this.labelTest03.Size = new System.Drawing.Size(72, 13);
+            this.labelTest03.TabIndex = 4;
+            this.labelTest03.Text = "Main Detail 3:";
+            // 
+            // labelTest02
+            // 
+            this.labelTest02.AutoSize = true;
+            this.labelTest02.Location = new System.Drawing.Point(19, 64);
+            this.labelTest02.Name = "labelTest02";
+            this.labelTest02.Size = new System.Drawing.Size(72, 13);
+            this.labelTest02.TabIndex = 2;
+            this.labelTest02.Text = "Main Detail 2:";
+            // 
+            // labelTest01
+            // 
+            this.labelTest01.AutoSize = true;
+            this.labelTest01.Location = new System.Drawing.Point(19, 31);
+            this.labelTest01.Name = "labelTest01";
+            this.labelTest01.Size = new System.Drawing.Size(72, 13);
+            this.labelTest01.TabIndex = 0;
+            this.labelTest01.Text = "Main Detail 1:";
+            // 
+            // groupBoxExtraDetail
+            // 
+            this.groupBoxExtraDetail.Controls.Add(this.textBox1);
+            this.groupBoxExtraDetail.Controls.Add(this.textBox2);
+            this.groupBoxExtraDetail.Controls.Add(this.textBox3);
+            this.groupBoxExtraDetail.Controls.Add(this.textBox4);
+            this.groupBoxExtraDetail.Controls.Add(this.label1);
+            this.groupBoxExtraDetail.Controls.Add(this.label2);
+            this.groupBoxExtraDetail.Controls.Add(this.label3);
+            this.groupBoxExtraDetail.Controls.Add(this.label4);
+            this.groupBoxExtraDetail.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxExtraDetail.Location = new System.Drawing.Point(2, 5);
+            this.groupBoxExtraDetail.Name = "groupBoxExtraDetail";
+            this.groupBoxExtraDetail.Size = new System.Drawing.Size(349, 251);
+            this.groupBoxExtraDetail.TabIndex = 0;
+            this.groupBoxExtraDetail.TabStop = false;
+            this.groupBoxExtraDetail.Text = "Extra Detail";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 119);
+            this.textBox1.MaxLength = 20;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(103, 86);
+            this.textBox2.MaxLength = 20;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(215, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(103, 53);
+            this.textBox3.MaxLength = 20;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(215, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(103, 24);
+            this.textBox4.MaxLength = 20;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(215, 20);
+            this.textBox4.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Extra Detail 8:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Extra Detail 7:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Extra Detail 6:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Extra Detail 5:";
+            // 
+            // FormLsgWinApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 303);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.panelExtra);
+            this.Controls.Add(this.panelButton);
+            this.Name = "FormLsgWinApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LSG Win App";
+            this.panelButton.ResumeLayout(false);
+            this.panelExtra.ResumeLayout(false);
+            this.panelMain.ResumeLayout(false);
+            this.groupBoxMainDetail.ResumeLayout(false);
+            this.groupBoxMainDetail.PerformLayout();
+            this.groupBoxExtraDetail.ResumeLayout(false);
+            this.groupBoxExtraDetail.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelButton;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Panel panelExtra;
+        private System.Windows.Forms.GroupBox groupBoxExtraDetail;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.GroupBox groupBoxMainDetail;
+        private System.Windows.Forms.TextBox textBox04;
+        private System.Windows.Forms.TextBox textBoxTest03;
+        private System.Windows.Forms.TextBox textBoxTest02;
+        private System.Windows.Forms.TextBox textBoxTest01;
+        private System.Windows.Forms.Label labelTest04;
+        private System.Windows.Forms.Label labelTest03;
+        private System.Windows.Forms.Label labelTest02;
+        private System.Windows.Forms.Label labelTest01;
+    }
+}
+
